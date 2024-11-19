599917b0f65dec53eaade1ddbf585cba86054839
qodl <jasperkreft@gmail.com>
2024-11-19 19:48:50 +0100

    Create example.py

---
+++ example.py
@@ -0,0 +1,4 @@
+from password_generator import * # Imports the password_generator script
+
+print("Your generated password is:", create_password(9)) # Prints out a password that is 9 characters long by calling the create_password(length) method from the imported script
+
